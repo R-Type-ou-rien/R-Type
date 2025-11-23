8fc69664d3e610c4de9a3f9733c289187e6ea89b
NoahKrummenacker <noah.krummenacker@epitech.eu>
2025-11-23 18:22:19 +0100

    feat(ECS): add component Sprite2D and InputControl

--- Poc-ECS/include/Components/Components.hpp
+++ Poc-ECS/include/Components/Components.hpp
@@ -7,10 +7,32 @@
 
 #pragma once
 
+#include <string>
+#include <SFML/Window/Keyboard.hpp>
+
 struct Position2D {
-    float x, y;
+    float x;
+    float y;
 };
 
 struct Velocity2D {
-    float vx, vy;
+    float vx;
+    float vy;
+};
+
+struct Sprite2D {
+    std::string texturePath;
+    int rectLeft;
+    int rectTop;
+    int rectWidth;
+    int rectHeight;
+    float scale = 1.0f;
+};
+
+struct InputControl {
+    sf::Keyboard::Key up;
+    sf::Keyboard::Key down;
+    sf::Keyboard::Key left;
+    sf::Keyboard::Key right;
+    float speed;
 };
