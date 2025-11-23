6eafd1cd898c6c3eef9800a5ba0a8a240934fe22
NoahKrummenacker <noah.krummenacker@epitech.eu>
2025-11-23 18:25:22 +0100

    feat(ECS): add ECS class for encapsulate all ellement like system  entity and component

---
+++ Poc-ECS/include/ECS.hpp
@@ -0,0 +1,53 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** ECS.hpp
+*/
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Registry.hpp"
+#include "SystemManager.hpp"
+
+class ECS {
+public:
+    ECS() : systems(registry) {}
+
+    ECS(unsigned int width = 800, unsigned int height = 600, const std::string& title = "R-Type")
+    : _window(sf::VideoMode(width, height), title), 
+    systems(registry) 
+    {
+        _window.setFramerateLimit(60);
+    }
+
+    void update(float dt) {
+        systems.updateAll(dt);
+    }
+
+    void run() {
+        sf::Clock clock;
+
+        while (_window.isOpen()) {
+            sf::Event event;
+            while (_window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed)
+                    _window.close();
+            }
+            sf::Time elapsed = clock.restart();
+            float dt = elapsed.asSeconds();
+            systems.updateAll(dt);
+        }
+    }
+
+    sf::RenderWindow& getWindow() {
+        return _window;
+    }
+
+    Registry registry;
+    SystemManager systems;
+
+private:
+    sf::RenderWindow _window;
+};
