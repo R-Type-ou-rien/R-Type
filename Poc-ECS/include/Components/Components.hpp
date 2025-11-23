35686856da65e249d006e41d1c8380d428ce316b
NoahKrummenacker <noah.krummenacker@epitech.eu>
2025-11-23 15:58:42 +0100

    feat(ECS): implement basic component Position2D ; float x, y Velocity2D : float x, y

---
+++ Poc-ECS/include/Components/Components.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** Components.hpp
+*/
+
+#pragma once
+
+struct Position2D {
+    float x, y;
+};
+
+struct Velocity2D {
+    float vx, vy;
+};
