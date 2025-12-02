9dbb988675b33dbb185f2c511427d38c9aa8a2a9
Boris <boris.cheng@epitech.eu>
2025-12-02 23:37:05 +0100

    feat: team component

---
+++ team_component/team_component.hpp
@@ -0,0 +1,13 @@
+#pragma one
+
+#ifndef TEAM_COMPONENT_TEAM_COMPONENT_HPP_
+  #define TEAM_COMPONENT_TEAM_COMPONENT_HPP_
+
+namespace ECS {
+struct TeamComponent {
+    enum Team { ALLY, ENEMY };
+    Team team;
+};
+}  // namespace ECS
+
+#endif  // TEAM_COMPONENT_TEAM_COMPONENT_HPP_
