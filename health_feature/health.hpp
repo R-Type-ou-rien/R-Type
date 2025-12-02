5464a45de4021961e72975ad92a3dde5837b516d
Boris <boris.cheng@epitech.eu>
2025-12-02 22:41:37 +0100

    feat: prototype of every function system and respect the cpplint

--- health_feature/health.hpp
+++ health_feature/health.hpp
@@ -4,20 +4,20 @@
 class Registry;
 
 namespace ECS {
-    struct HealthComponent {
-        int max_hp;
-        int current_hp;
-    };
+struct HealthComponent {
+    int max_hp;
+    int current_hp;
+};
 
-    class HealthSystem {
-    public:
-        HealthSystem() = default;
-        ~HealthSystem() = default;
-        void update();
+class HealthSystem {
+   public:
+    HealthSystem() = default;
+    ~HealthSystem() = default;
+    void update(Registry& registry);
 
-    private:
-        void check_death(Entity e);
-        void handle_enemy_death(Entity e);
-        void handle_player_death(Entity e);
-    }
-}
+   private:
+    void check_death(Registry& registry); //, Entity e);
+    void handle_enemy_death(Registry& registry); //, Entity e);
+    void handle_player_death(Registry& registry); //, Entity e);
+};
+}  // namespace ECS
