cde1ba6e1fab2de27ebae5c530e0a62e342b1540
Boris <boris.cheng@epitech.eu>
2025-12-02 23:39:58 +0100

    fix: cpplint and clang-format for the team component

--- team_component/team_component.hpp
+++ team_component/team_component.hpp
@@ -1,7 +1,7 @@
 #pragma one
 
 #ifndef TEAM_COMPONENT_TEAM_COMPONENT_HPP_
-  #define TEAM_COMPONENT_TEAM_COMPONENT_HPP_
+#define TEAM_COMPONENT_TEAM_COMPONENT_HPP_
 
 namespace ECS {
 struct TeamComponent {
