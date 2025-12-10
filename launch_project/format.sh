bd8f3674487a308291a66d0e4c31a877e78f403b
Boris <boris.cheng@epitech.eu>
2025-12-10 22:49:49 +0100

    fix: clang format for include in network file

--- launch_project/format.sh
+++ launch_project/format.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "Formattage du code..."
-find src tests -type f \( -name "*.cpp" -o -name "*.hpp" \) \
-    ! -path "*/build/*" \
-    ! -path "*/vcpkg/*" \
+
+find src tests Network test_input -type f \( -name "*.cpp" -o -name "*.hpp" \) \
     -exec clang-format -i -style=file {} +
-echo "Terminé."
