18be6d34cbc03e1bb8f1128af1ac808bd3e0572e
vignesh <vignesh.mourouganandame@epitech.eu>
2025-12-15 22:58:22 +0100

    fix: name of event and erase network folder

--- src/Engine/Core/Network/Client/Client.cpp
+++ src/Engine/Core/Network/Client/Client.cpp
@@ -10,7 +10,7 @@
 #include "../Network.hpp"
 
 void Client::PingServer() {
-    AddMessageToServer(RTypeEvents::C_PING_SERVER, 0, NULL);
+    AddMessageToServer(GameEvents::C_PING_SERVER, 0, NULL);
 }
 
 void Client::LoginServerToken() {
@@ -19,7 +19,7 @@ void Client::LoginServerToken() {
     if (file.good()) {
         std::getline(file, token);
         file.close();
-        AddMessageToServer(RTypeEvents::C_LOGIN_TOKEN, 0, token);
+        AddMessageToServer(GameEvents::C_LOGIN_TOKEN, 0, token);
     }
 }
 
@@ -28,7 +28,7 @@ void Client::LoginServer(std::string username, std::string password) {
     std::strncpy(info.username, username.c_str(), sizeof(info.username));
     std::strncpy(info.password, password.c_str(), sizeof(info.password));
 
-    AddMessageToServer(RTypeEvents::C_LOGIN, 0, info);
+    AddMessageToServer(GameEvents::C_LOGIN, 0, info);
 }
 
 void Client::RegisterServer(std::string username, std::string password) {
@@ -36,7 +36,7 @@ void Client::RegisterServer(std::string username, std::string password) {
     std::strncpy(info.username, username.c_str(), sizeof(info.username));
     std::strncpy(info.password, password.c_str(), sizeof(info.password));
 
-    AddMessageToServer(RTypeEvents::C_REGISTER, 0, info);
+    AddMessageToServer(GameEvents::C_REGISTER, 0, info);
 }
 
 coming_message Client::ReadIncomingMessage() {
@@ -44,28 +44,10 @@ coming_message Client::ReadIncomingMessage() {
     if (!Incoming().empty()) {
         auto msg = Incoming().pop_front();
 
-        if (msg.msg.header.id == RTypeEvents::S_REGISTER_OK || msg.msg.header.id == RTypeEvents::S_LOGIN_OK) {
-            auto now = std::chrono::system_clock::now();
-            auto expiration_time = now + std::chrono::hours(10 * 24);
-            auto expiration_timestamp = std::chrono::system_clock::to_time_t(expiration_time);
-
-            connection_server_return response;
-            auto tempMsg = msg.msg;
-            tempMsg >> response;
-
-            std::ofstream file(TOKEN_FILENAME);
-            if (file.is_open()) {
-                file << response.token;
-                file.close();
-            }
-            std::ofstream expiration_file(static_cast<std::string>(TOKEN_FILENAME) + ".expire");
-            expiration_file << expiration_timestamp;
-        } else {
-            coming_message comingMsg;
-            comingMsg.id = msg.msg.header.id;
-            msg.msg >> comingMsg.msg.body;
-            return comingMsg;
-        }
+        coming_message comingMsg;
+        comingMsg.id = msg.msg.header.id;
+        msg.msg >> comingMsg.msg.body;
+        return comingMsg;
     }
     return coming_message{};
 };
