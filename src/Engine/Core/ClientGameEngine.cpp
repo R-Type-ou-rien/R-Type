5ba73d019ae75e2498046f206e32de97d92309d7
mattXmatt <matthias.balon@epitech.eu>
2025-12-29 17:06:28 +0100

    feat: the input manager is now logically handle by the (client) game engine instead of the ecs

--- src/Engine/Core/ClientGameEngine.cpp
+++ src/Engine/Core/ClientGameEngine.cpp
@@ -7,30 +7,29 @@
 ClientGameEngine::ClientGameEngine(std::string window_name) : _window_manager(WINDOW_W, WINDOW_H, window_name) {}
 
 int ClientGameEngine::init() {
-    InputManager input_manager;
 
     _ecs.systems.addSystem<BackgroundSystem>();
     _ecs.systems.addSystem<SpawnSystem>();
     _ecs.systems.addSystem<RenderSystem>();
-    _ecs.systems.addSystem<InputSystem>(_ecs.input);
+    _ecs.systems.addSystem<InputSystem>(input_manager);
     _ecs.systems.addSystem<BoxCollision>();
     _ecs.systems.addSystem<ActionScriptSystem>();
     _ecs.systems.addSystem<PatternSystem>();
 
     if (_init_function)
-        _init_function(_ecs);
+        _init_function(_ecs, input_manager);
 
     // if mode local
     _ecs.systems.addSystem<PhysicsSystem>();
     return 0;
 }
 
-void ClientGameEngine::setUserFunction(std::function<void(ECS& ecs)> user_function) {
+void ClientGameEngine::setUserFunction(std::function<USER_FUNCTION_TYPE> user_function) {
     _function = user_function;
     return;
 }
 
-void ClientGameEngine::setInitFunction(std::function<void(ECS& ecs)> user_function) {
+void ClientGameEngine::setInitFunction(std::function<USER_FUNCTION_TYPE> user_function) {
     _init_function = user_function;
     return;
 }
@@ -40,15 +39,15 @@ void ClientGameEngine::handleEvent() {
         if (event->is<sf::Event::Closed>())
             _window_manager.getWindow().close();
         if (event->is<sf::Event::FocusLost>())
-            _ecs.input.setWindowHasFocus(false);
+            input_manager.setWindowHasFocus(false);
         if (event->is<sf::Event::FocusGained>())
-            _ecs.input.setWindowHasFocus(true);
+            input_manager.setWindowHasFocus(true);
     }
 }
 
 int ClientGameEngine::run() {
     sf::Clock clock;
-    system_context context = {0, _ecs._textureManager, _window_manager.getWindow(), _ecs.input};
+    system_context context = {0, _ecs._textureManager, _window_manager.getWindow(), input_manager};
 
     this->init();
     while (_window_manager.isOpen()) {
@@ -56,7 +55,7 @@ int ClientGameEngine::run() {
         handleEvent();
         _window_manager.clear();
         if (_function)
-            _function(_ecs);
+            _function(_ecs, input_manager);
         _ecs.update(context);
         _window_manager.display();
     }
--- src/Engine/Core/ClientGameEngine.hpp
+++ src/Engine/Core/ClientGameEngine.hpp
@@ -7,6 +7,7 @@
 
 #include "ECS/ECS.hpp"
 #include "ECS/ISystem.hpp"
+#include "InputManager.hpp"
 #include "InputSystem.hpp"
 #include "PhysicsSystem.hpp"
 #include "RenderSystem.hpp"
@@ -19,13 +20,15 @@
 #define FAILURE -1
 #define WINDOW_H 1000
 #define WINDOW_W 1000
+#define USER_FUNCTION_TYPE void(ECS& ecs, InputManager& inputs)
 
 class ClientGameEngine {
    private:
     ECS _ecs;
     WindowManager _window_manager;
-    std::function<void(ECS& ecs)> _function;
-    std::function<void(ECS& ecs)> _init_function;
+    InputManager input_manager;
+    std::function<USER_FUNCTION_TYPE> _function;
+    std::function<USER_FUNCTION_TYPE> _init_function;
     // client network class
 
    public:
@@ -33,8 +36,8 @@ class ClientGameEngine {
     int run();
     explicit ClientGameEngine(std::string window_name = "Default Name");
     ~ClientGameEngine() {}
-    void setUserFunction(std::function<void(ECS& ecs)> user_function);
-    void setInitFunction(std::function<void(ECS& ecs)> user_function);
+    void setUserFunction(std::function<USER_FUNCTION_TYPE> user_function);
+    void setInitFunction(std::function<USER_FUNCTION_TYPE> user_function);
 
    private:
     void handleEvent();
