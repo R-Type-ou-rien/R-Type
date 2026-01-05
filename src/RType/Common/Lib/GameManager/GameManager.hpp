613067b71cb2c4d74b170ff17b93d0de9e79a7b5
mikal zheng <mikal.zheng@epitech.eu>
2026-01-05 16:51:52 +0100

    feat: Add ChargedShot and PenetratingProjectile components; update ShooterSystem for charged projectiles

--- src/RType/Common/Lib/GameManager/GameManager.hpp
+++ src/RType/Common/Lib/GameManager/GameManager.hpp
@@ -7,9 +7,24 @@
 
 class GameManager {
    private:
+    static constexpr float PLAYER_SPEED = 100.0f;
+    static constexpr float PLAYER_START_X = 100.0f;
+    static constexpr float PLAYER_START_Y = 300.0f;
+    static constexpr int PLAYER_MAX_HP = 100;
+    
     std::unique_ptr<Player> _player;
     std::vector<std::unique_ptr<AI>> _ennemies;
     Entity _uiEntity;
+
+    void initSystems(ECS& ecs);
+    void initBackground(ECS& ecs, ResourceManager<TextureAsset>& textures);
+    void initPlayer(ECS& ecs, ResourceManager<TextureAsset>& textures);
+    void initEnemies(ECS& ecs, ResourceManager<TextureAsset>& textures);
+    void initSpawner(ECS& ecs);
+    void initUI(ECS& ecs);
+    void setupMovementControls(InputManager& inputs);
+    void setupShootingControls(InputManager& inputs);
+    void updateUI(ECS& ecs);
 
    public:
     GameManager();
