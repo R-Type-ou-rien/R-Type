38338024763fd44d68502a07fad7a7e4ede24cfe
NoahKrummenacker <noah.krummenacker@epitech.eu>
2026-01-14 14:43:34 +0100

    feat: implement a new sprite component for sprite2D with layer of display this new component is for separate the older sprite2d component beacose the older is use for static sprite and animated sprite now this sprite is juste use for static and he have a layer features

---
+++ src/Engine/Lib/Components/StructDatas/RenderLayer.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** Smash
+** File description:
+** RenderLayer.hpp
+*/
+
+#pragma once
+
+enum class RenderLayer {
+    Background = 0,
+    Midground = 1,
+    Foreground = 2,
+    UI = 3
+};
