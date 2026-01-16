699c7e3fcfdd1cd070a26106792cdf7332715f7a
mattXmatt <matthias.balon@epitech.eu>
2026-01-16 16:06:46 +0100

    fix: includes for windows are now implemented

--- src/Network/Server/main.cpp
+++ src/Network/Server/main.cpp
@@ -1,8 +1,20 @@
 #include "Server.hpp"
 #include <iostream>
-#include <ifaddrs.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#ifdef _WIN32
+    #define WIN32_LEAN_AND_MEAN
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #include <iphlpapi.h>
+    #pragma comment(lib, "Ws2_32.lib")
+    #pragma comment(lib, "Iphlpapi.lib")
+
+#else
+    #include <ifaddrs.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <sys/types.h>
+    #include <sys/socket.h>
+#endif
 
 int main() {
     std::cout << std::unitbuf;
